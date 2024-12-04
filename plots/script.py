426d0bebab06ca3fcd9512f43aa18a0b34767ea1
Soumil Gupta <soumil.gupta@students.iiit.ac.in>
2024-12-04 16:48:31 +0530

    [+] final plots

--- plots/script.py
+++ plots/script.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 # Sample data for BiPO
 x = [2, 3, 5, 8 ,13]
-y_case1 = [2.1, 1.3, 2.2, 0.4, 1.0]  # Replace with your actual BiPO data points
+y_case1 = [6.8, 6.1, 5.9, 6.7, 5.9]  # Replace with your actual BiPO data points
 
 # Create a scatter plot
 sns.lineplot(x=x, y=y_case1, marker='o')
@@ -11,11 +11,11 @@ sns.lineplot(x=x, y=y_case1, marker='o')
 # Add labels, title, and legend
 plt.xlabel('Layer Number', fontsize=14)
 plt.ylabel('Average Behavioral Score', fontsize=14)
-plt.title('Coherence across layers with multiplier +1', fontsize=16)
+plt.title('Refusal across layers with multiplier -1', fontsize=16)
 plt.xticks(ticks=x)
 
 # Save the plot in the specified folder
-plt.savefig('layer_wise_plus_1.png')  # Replace with your desired folder path
+plt.savefig('layer_wise_refusal_minus_1.png')  # Replace with your desired folder path
 
 # Show the plot (optional)
 plt.show()
